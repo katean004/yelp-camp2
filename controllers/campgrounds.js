0c46cd892aa87cadc6571531c483d128b1ea88c6
kate <kan004@ucr.edu>
2021-10-13 15:43:06 -0700

    added bootstrap styling to image files button

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -18,7 +18,6 @@ module.exports.createCamp = async (req, res, next) => {
   }));
   campground.author = req.user._id;
   await campground.save();
-  console.log(campground);
   req.flash("success", "Successfully created new campground");
   res.redirect(`/campgrounds/${campground._id}`);
 };
