11498a7bb8b85bd37024b8af011db33829505c4c
kate <kan004@ucr.edu>
2021-10-14 16:10:23 -0700

    fixed styling on show page

--- public/scripts/showMap.js
+++ public/scripts/showMap.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  style: "mapbox://styles/mapbox/outdoors-v11", // style URL
   center: campground.geometry.coordinates, // [lng, lat]
   zoom: 9 // starting zoom
 });
@@ -10,4 +10,5 @@ const map = new mapboxgl.Map({
 // Create a default Marker and add it to the map.
 const marker1 = new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
+  .setPopup(new mapboxgl.Popup().setHTML(`<h3>${campground.title}</h3>`))
   .addTo(map);
